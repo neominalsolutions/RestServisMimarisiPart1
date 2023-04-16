308bae768561b02ab881a5bb091dfb351dc5463c
mert.alptekin <mert.alptekin@neomina.com>
2023-04-16 23:45:49 +0300

    Add project files.

---
+++ RestServisMimarisiPart1/Program.cs
@@ -0,0 +1,51 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// MVC Şablonu kullanacağımızdan Controllers Service'i Projemize dahi ettik.
+// Artık uygulamamızda Controllers adında bir klasör açıp gelen web isteklerini  işleyebiliriz.
+builder.Services.AddControllers(); 
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+// Net 6.0 ile gelen Minimal API yöntemine bir örnek
+app.MapGet("/weatherforecast", () =>
+{
+  var forecast = Enumerable.Range(1, 5).Select(index =>
+      new WeatherForecast
+      (
+          DateTime.Now.AddDays(index),
+          Random.Shared.Next(-20, 55),
+          summaries[Random.Shared.Next(summaries.Length)]
+      ))
+      .ToArray();
+  return forecast;
+})
+.WithName("GetWeatherForecast");
+
+// gelen web isteklerin Minimal API yöntemi dışında, controller üzerinden karşılanabilmesi için middleware sürecimize ekledik.
+app.MapControllers(); 
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
